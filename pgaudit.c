983b3db6de8474364443d3e2dc25152cbbaeb243
Laurenz Albe <laurenz.albe@cybertec.at>
2025-04-30 18:08:57 +0200

    Fix bug that prevented logging to the client
    This was unnecessary and made the regression tests emit nothing.
    
    Also, don't log row counts for forced superuser logging.
    
    Taken from a bigger patch by Christoph Berg.

--- pgaudit.c
+++ pgaudit.c
@@ -868,7 +868,7 @@ log_audit_event(AuditEventStackItem *stackItem)
                                "<previously logged>,<previously logged>");
 
     /* Log rows affected */
-    if (auditLogRows || am_superuser)
+    if (auditLogRows)
         appendStringInfo(&auditStr, "," INT64_FORMAT,
                          stackItem->auditEvent.rows);
 
@@ -877,7 +877,7 @@ log_audit_event(AuditEventStackItem *stackItem)
      * translatability, but we currently haven't got translation support in
      * pgaudit anyway.
      */
-    ereport((auditLogClient && !am_superuser) ? auditLogLevel : LOG_SERVER_ONLY,
+    ereport(auditLogClient ? auditLogLevel : LOG_SERVER_ONLY,
             (errmsg("AUDIT: %s," INT64_FORMAT "," INT64_FORMAT ",%s,%s",
                     stackItem->auditEvent.granted ?
                     AUDIT_TYPE_OBJECT : AUDIT_TYPE_SESSION,
@@ -1484,13 +1484,12 @@ static bool
 pgaudit_ExecutorCheckPerms_hook(List *rangeTabls, List *permInfos, bool abort)
 {
     Oid auditOid;
-    bool am_superuser = is_real_superuser();
 
     /* Get the audit oid if the role exists */
     auditOid = get_role_oid(auditRole, true);
 
     /* Log DML if the audit role is valid or session logging is enabled */
-    if ((auditOid != InvalidOid || auditLogBitmap != 0 || am_superuser) &&
+    if ((auditOid != InvalidOid || auditLogBitmap != 0 || is_real_superuser()) &&
         !IsAbortedTransactionBlockState())
     {
         /* If auditLogRows is on, wait for rows processed to be set */
